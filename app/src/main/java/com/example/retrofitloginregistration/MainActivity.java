17e95cfe014ea41f6ad2cf5d6ab33b4804354783
zack-mwangi <zackmwangi998@gmail.com>
2021-09-05 22:19:44 +0300

    second Commit

---
+++ app/src/main/java/com/example/retrofitloginregistration/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.retrofitloginregistration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(getApplicationContext(), "Ayee", Toast.LENGTH_SHORT).show();
+    }
+}
